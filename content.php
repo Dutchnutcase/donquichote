8ea37289e73ad448d1c43eeb9b194bd2da6ff84c
root <root@apollo.speelmanrobben.nl>
2018-07-08 21:01:29 +0200

    Added some indentations and removed spurious functions.

--- content.php
+++ content.php
@@ -1,15 +1,8 @@
 <div class="row">
-<div style="margin-left: 20px;" >
-<div style="padding-bottom: 10px;" >
-<h2 ><?php the_title(); ?></h2>
-<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-</div>
-
-
-<?php the_content(); ?>
-
-
-
-
-</div>
+  <div style="margin-left: 20px;" >
+    <div style="padding-bottom: 10px;" >
+      <h2 ><?php the_title(); ?></h2>
+    </div>
+    <?php the_content(); ?>
+  </div>
 </div>
--- functions.php
+++ functions.php
@@ -3,11 +3,6 @@
   {
     register_nav_menu('header-menu',__( 'Header Menu' ));
   }
-  function plugin_myContentFilter($content)
-  {
-    return substr($content, 0, 300);
-  }
   add_action( 'init', 'register_dq_menu' );
   add_theme_support( 'post-thumbnails' );
-  add_filter("the_content", "plugin_myContentFilter");
 ?>
