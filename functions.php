a0b36a421baebaf4ca1a064b1221f4e791b8d0e6
root <root@apollo.speelmanrobben.nl>
2018-08-10 21:55:52 +0200

    Adds optional dynamic sidebar for #4

--- functions.php
+++ functions.php
@@ -5,4 +5,13 @@
   }
   add_action( 'init', 'register_dq_menu' );
   add_theme_support( 'post-thumbnails' );
+  if ( function_exists('register_sidebar') )
+  register_sidebar(array(
+    'name' => 'Sidebar area',
+    'before_widget' => '<div class = "widgetizedArea">',
+    'after_widget' => '</div>',
+    'before_title' => '<h3>',
+    'after_title' => '</h3>',
+  )
+);
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <div class="col-lg-3">
 <div class="side" style="margin-top: 40px;">
 <?php dynamic_sidebar('smartslider_area_1'); ?>
-<a href="https://publiek.usc.ru.nl/info/?browser=on&lang=nl&menu=verberg&mode=activiteit&type=aanbod&standaard=&naam=Schermen">
-<img src="<?php echo get_bloginfo('template_directory'); ?>/img/rsclink.gif" alt="rsclink" class="img-rounded-mid"></a>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar area") ) : ?>
+<?php endif;?>
 </div>
 
 <div class="col-lg-1">
