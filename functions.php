c0cf20560d956223402dfb69121e4688bf7eff27
root <root@apollo.speelmanrobben.nl>
2018-08-29 11:02:33 +0200

    Added subpages view to sidebar.

--- functions.php
+++ functions.php
@@ -12,6 +12,23 @@
       'after_widget' => '</div>',
       'before_title' => '<h3>',
       'after_title' => '</h3>',
-    )
-  );
+    ));
+  function wpb_list_child_pages()
+  {
+    global $post;
+    if ( is_page() && $post->post_parent )
+    {
+      $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0' );
+    }
+    else
+    {
+      $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0' );
+    }
+    if ( $childpages )
+    {
+      $string = '<ul>' . $childpages . '</ul>';
+    }
+    return $string;
+  }
+  add_shortcode('wpb_childpages', 'wpb_list_child_pages');
 ?>
