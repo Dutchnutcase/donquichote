637690fe60dc83479e48ae8da94d94cd677a8529
Nico Speelman <nico@SPEELMANROBBEN.NL>
2018-09-20 21:01:20 +0200

    Added translation files.

--- posts_category.php
+++ posts_category.php
@@ -14,7 +14,7 @@
 <div class="post hentry ivycat-post">
 	<!-- 	This outputs the post TITLE -->
 	<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-	<p class="blog-post-meta"><?php the_date(); ?>, by <?php the_author_posts_link(); ?></p>
+	<p class="blog-post-meta"><?php the_date(); ?>, <?php _e( 'by', 'donquichote' ); ?> <?php the_author_posts_link(); ?></p>
 
 	<!-- 	This outputs the post EXCERPT.  To display full content including images and html,
 		replace the_excerpt(); with the_content();  below. -->
